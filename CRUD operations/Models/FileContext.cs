c196c9872caaa8dd834a9ea008185e8fcd8149a2
blazhkevych.v_2 <blazhkevych.v@gmail.com>
2023-09-22 13:33:42 +0300

    #create view done

---
+++ CRUD operations/Models/FileContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_operations.Models
+{
+    public class FileContext : DbContext
+    {
+        public DbSet<FileModel> Files { get; set; }
+
+        public FileContext(DbContextOptions<FileContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+
+
+}
